304aeb0d1f8e411fed76dac195991d526b97c7e7
Egor <29282634+FlashgoKless@users.noreply.github.com>
2022-09-25 21:27:57 +0300

    Add lab1

---
+++ lab1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+    int a;
+    std::cout << "Input value of variable A: ";
+    std::cin >> a;
+    std::cout << "A^2: ";
+    std::cout << a * a;
+    std::cout << "\nA^3: ";
+    std::cout << a * a * a;
+    std::cout << "\nA^5: ";
+    int a1 = a * a * a * a * a;
+    std::cout << a1;
+    std::cout << "\nA^10: ";
+    int a2 = a1 * a * a * a * a * a;
+    std::cout << a2;
+    std::cout << "\nA^15: ";
+    std::cout << a2 * a * a * a * a * a;
+}
