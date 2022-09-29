e02463845e7b258a17f268d48da9bd7173374e7e
Egor <29282634+FlashgoKless@users.noreply.github.com>
2022-09-29 12:05:14 +0300

    Add lab6

---
+++ lab6.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+    int N;
+    std::cout << "Enter integer number: ";
+    std::cin >> N;
+    double A = 2;
+    for (int i = 1; i <= N; ++i) {
+        A = 2 + 1 / A;
+        std::cout << A << " ";
+    }
+    std::cout << std::endl;
+    system("pause");
+    return 0;
+}
