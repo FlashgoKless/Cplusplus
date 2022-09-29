6ba34a9cd2c9f1ef24cf2bd36c988f6e355c2f40
Egor <29282634+FlashgoKless@users.noreply.github.com>
2022-09-29 12:10:45 +0300

    Add lab7

---
+++ lab7.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+    float e;
+    std::cout << "Enter number e:";
+    std::cin >> e;
+    float ak1 = 1, ak = 2, ak2;
+    int k = 2;
+    while (fabs(ak - ak1) >= e) {
+        ak2 = ak1;
+        ak1 = ak;
+        ak = (ak2 + 2 * ak1) / 3;
+        ++k;
+    }
+    std::cout << "k:" << k << std::endl << "A(k-1):" << ak1 << std::endl << "Ak:" << ak;
+
+    return 0;
+}
+
