0512964203cbbe2835b0f1e401e8e4fad72533b6
Egor <29282634+FlashgoKless@users.noreply.github.com>
2022-10-03 13:54:12 +0300

    Add labs 8, 9.

---
+++ lab8.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main(void)
+{
+    int a[10], b[10];
+    int n;
+
+    printf("N: ");
+    scanf("%i", &n);
+
+    int k;
+    for (k = 0; k < n; ++k) {
+        printf("a[%i] : ", k + 1);
+        scanf("%i", &a[k]);
+    }
+
+    int i = -1;
+    for (k = 0; k < n; k += 2) {
+        ++i;
+        b[i] = a[k];
+    }
+
+    printf("B: \n size : %i\n", ++i);
+    for (k = 0; k < i; ++k) printf("  %i: %i\n", k + 1, b[k]);
+
+    return 0;
+}
---
+++ lab9.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+int main(void)
+{
+    int a[10][10];
+    int m, n;
+
+    printf("M: ");
+    scanf("%i", &m);
+    printf("N: ");
+    scanf("%i", &n);
+
+
+
+    int i, j;
+    for (i = 0; i < m; ++i) {
+        printf("%i : \n", i + 1);
+        for (j = 0; j < n; ++j) {
+            printf("%i : ", j + 1);
+            scanf("%i", &a[i][j]);
+        }
+    }
+
+    int column, num;
+    for (j = n - 1; j >= 0; --j) {
+        num = 0;
+        for (i = 0; i < m; ++i)
+            if (a[i][j] < 0) num++;
+        if (num == m) {
+            column = j;
+            break;
+        }
+
+    }
+
+
+    --n;
+    for (j = column; j < n; ++j)
+        for (i = 0; i < m; ++i)
+            a[i][j] = a[i][j + 1];
+
+
+    for (i = 0; i < m; ++i) {
+        for (j = 0; j < n; ++j) printf(" : %i", a[i][j]);
+        printf(" : \n");
+    }
+    return 0;
+}
