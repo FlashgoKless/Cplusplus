d3e0c01fb2e25942cdbd510ca4b6a127a059bafd
Egor Romenko <29282634+FlashgoKless@users.noreply.github.com>
2022-10-10 23:31:33 +0300

    Add lab11

---
+++ ex11.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    string s;
+    int i, num;
+    char char_1 = ',';
+    char char_2 = '.';
+    char char_3 = ':';
+    char char_4 = ';';
+    char char_5 = '!';
+    char char_6 = '?';
+
+    cout << "Введите строку: \n";
+    cin >> s;
+
+    num = 0;
+
+    for (i = 0; i <= s.length(); i++) {
+        if (s[i] == char_1) {
+            num++;
+        }
+        else if (s[i] == char_2) {
+            num++;
+        }
+        else if (s[i] == char_3) {
+            num++;
+        }
+        else if (s[i] == char_4) {
+            num++;
+        }
+        else if (s[i] == char_5) {
+            num++;
+        }
+        else if (s[i] == char_6) {
+            num++;
+        }
+    }
+
+    cout << "Число знаков препинания: " << num;
+}
