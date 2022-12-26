e25f32a5bb7dce3c533a683b9dfc64eb2035225c
Egor Romenko <29282634+FlashgoKless@users.noreply.github.com>
2022-12-26 13:52:02 +0300

    Add ex16

---
+++ ex16/ex16.cpp
@@ -0,0 +1,58 @@
+﻿#include "Time.h"
+
+using namespace std;
+
+int main() {
+	Triad p;
+	Time c;
+
+	cout << p << "\n";
+	cout << c << "\n";
+
+	p.set_first(22);
+	p.set_second(23);
+	p.set_third(24);
+
+	c.set_first(22);
+	c.set_second(23);
+	c.set_third(24);
+
+	cout << p << "\n";
+	cout << c << "\n\n";
+
+	int x, y, z;
+	cout << "Input three number: " << endl;
+	cin >> x >> y >> z;
+	cout << "\n";
+
+	if (x < 0 || y < 0 || z < 0) {
+		cout << "Error. Number can't be < 0." << endl;
+		return 0;
+	}
+
+	Triad m(x, y, z);
+	Time n(x, y, z);
+
+	cout << "First: " << m.get_first() << "; second: " << m.get_second() << "; third: " << m.get_third() << "." << "\n" << endl;
+
+	m.add_first();
+	m.add_second();
+	m.add_third();
+
+	cout << "String: " << m.toString() << "\n" << endl;
+
+	n.normalize_time();
+	n.get_time();
+
+	n.add_first();
+	n.add_second();
+	n.add_third();
+
+	cout << n << "\n";
+
+	n.add_seconds(20);
+	n.add_minute(70);
+	n.normalize_time();
+
+	cout << "String: " << n.toString() << endl;
+}
